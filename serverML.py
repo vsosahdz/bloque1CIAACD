879e93c1b3d17d3fb3b7813f2dbd988ae2a6d937
Víctor Adrián <victoradrian@Administrators-MacBook-Air-9.local>
2024-08-27 18:52:50 -0600

    Servidor en python-flask

---
+++ serverML.py
@@ -0,0 +1,31 @@
+#Python Libraries
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+from load import joblib
+# Files management
+import os
+from werkzeug.utils import secure_filename
+
+# Load model
+dt=joblib.load('dt1_ml.joblib')
+# Create Flask app
+server = Flask(__name__)
+
+# Define a route to send JSON data
+@server.route('/predictjson', methods=['POST'])
+def predictjson():
+    #Procesar datos de entrada
+    data = request.json
+    print(data)
+    inputData = np.array([
+        data['pH'],
+        data['sulphates'],
+        data['alcohol']
+    ])
+    #Predicir utilizando la entrada y el modelo
+    result = dt.predict(inputData.reshape(1, -1))
+    #Enviar respuesta
+    return jsonify({'Prediction': str(result[0])})
+
+if __name__ == '__main__':
+    server.run(debug=False,host='0.0.0.0',port=8080)
