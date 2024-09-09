6722e67148ce28c71d38f0d49dc50c0257d2463a
Víctor Adrián Sosa Hernández <47755333+vsosahdz@users.noreply.github.com>
2024-09-09 17:35:11 -0600

    code change 2

--- serverML.py
+++ serverML.py
@@ -27,5 +27,48 @@ def predictjson():
     #Enviar respuesta
     return jsonify({'Prediction': str(result[0])})
 
+#forms
+@server.route("/formulario",methods=['GET'])
+def formulario():
+    return render_template('pagina.html')
+
+#Envio de datos a través de Archivos
+@server.route('/modeloFile', methods=['POST'])
+def modeloFile():
+    f = request.files['file']
+    filename=secure_filename(f.filename)
+    path=os.path.join(os.getcwd(),'files',filename)
+    f.save(path)
+    file = open(path, "r")
+    
+    for x in file:
+        info=x.split()
+    print(info)
+    datosEntrada = np.array([
+            float(info[0]),
+            float(info[1]),
+            float(info[2])
+        ])
+    #Utilizar el modelo
+    resultado=dt.predict(datosEntrada.reshape(1,-1))
+    #Regresar la salida del modelo
+    return jsonify({"Resultado":str(resultado[0])})
+
+#Envio de datos a través de Forms
+@server.route('/modeloForm', methods=['POST'])
+def modeloForm():
+    #Procesar datos de entrada 
+    contenido = request.form
+    
+    datosEntrada = np.array([
+            contenido['pH'],
+            contenido['sulphates'],
+            contenido['alcohol']
+        ])
+    #Utilizar el modelo
+    resultado=dt.predict(datosEntrada.reshape(1,-1))
+    #Regresar la salida del modelo
+    return jsonify({"Resultado":str(resultado[0])})
+
 if __name__ == '__main__':
     server.run(debug=False,host='0.0.0.0',port=8080)
